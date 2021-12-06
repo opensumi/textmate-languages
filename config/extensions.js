82f7f043f1f47b6674a4f65593d76919eb8aee34
ensorrow <zheyanglv@qq.com>
2021-12-06 14:21:16 +0800

    chore: 迁移到 open-vsx

--- config/extensions.js
+++ config/extensions.js
@@ -1,116 +1,75 @@
 module.exports = {
-  "extensions": {
-    "vscode-extensions": [
+  extensions: {
+    vscode: [
       {
-        "name": "html",
-        "version": "1.0.0"
+        name: 'vue',
+        version: '0.1.5'
       },
       {
-        "name": "css",
-        "version": "1.0.0"
+        name: 'acss',
+        version: '0.0.2'
       },
       {
-        "name": "json",
-        "version": "1.0.0"
+        name: 'schema',
+        version: '0.0.1'
       },
       {
-        "name": "less",
-        "version": "1.0.0"
+        name: 'velocity',
+        version: '0.7.1'
       },
       {
-        "name": "scss",
-        "version": "1.0.0"
+        name: 'vscode-proto3',
+        version: '0.3.0'
       },
       {
-        "name": "markdown-basics",
-        "version": "1.0.0"
-      },
-      {
-        "name": "go",
-        "version": "1.0.0"
-      },
-      {
-        "name": "java",
-        "version": "1.0.0"
-      },
-      {
-        "name": "python-basic",
-        "version": "1.0.0"
-      },
-      {
-        "name": "vue",
-        "version": "0.1.5"
-      },
-      {
-        "name": "acss",
-        "version": "0.0.2"
-      },
-      {
-        "name": "schema",
-        "version": "0.0.1"
-      },
-      {
-        "name": "velocity",
-        "version": "0.7.1"
-      },
-      {
-        "name": "vscode-proto3",
-        "version": "0.3.0"
-      },
-      {
-        "name": "javascript",
-        "version": "1.0.0"
-      },
-    ],
-    "kaitian": [
-      {
-        "name": "typescript-basics",
-        "version": "1.51.1-patch.1"
+        name: 'typescript-basics',
+        version: '1.60.0'
       }
-    ],
-    "AONE-IDE": [
-      "bat",
-      "clojure",
-      "coffeescript",
-      "cpp",
-      "csharp",
-      // "css",
-      "docker",
-      "fsharp",
-      // "go",
-      "groovy",
-      "handlebars",
-      "hlsl",
-      // "html",
-      "ini",
-      // "java",
-      // "javascript",
-      // "json",
-      // "less",
-      "log",
-      "lua",
-      "make",
-      // "markdown-basics",
-      "objective-c",
-      "perl",
-      "php",
-      "powershell",
-      "pug",
-      // "r", 暂时不支持
-      "razor",
-      "ruby",
-      "rust",
-      // "scss",
-      "shaderlab",
-      "shellscript",
-      "sql",
-      "swift",
-      // "typescript-basics",
-      "vb",
-      "xml",
-      "yaml",
-    ].map(name => ({
-      name,
-    })),
+    ].concat(
+      [
+        'bat',
+        'clojure',
+        'coffeescript',
+        'cpp',
+        'csharp',
+        'css',
+        'docker',
+        'fsharp',
+        'go',
+        'groovy',
+        'handlebars',
+        'hlsl',
+        'html',
+        'ini',
+        'java',
+        'javascript',
+        'json',
+        'less',
+        'log',
+        'lua',
+        'make',
+        'markdown-basics',
+        'objective-c',
+        'perl',
+        'php',
+        'powershell',
+        'pug',
+        'python',
+        'razor',
+        'ruby',
+        'rust',
+        'scss',
+        'shaderlab',
+        'shellscript',
+        'sql',
+        'swift',
+        'vb',
+        'xml',
+        'yaml'
+      ].map(name => ({
+        name,
+        version: '1.55.2'
+      }))
+    )
   }
 }
--- scripts/update-extensions.js
+++ scripts/update-extensions.js
@@ -1,22 +1,39 @@
 const path = require('path')
 const rimraf = require('rimraf')
 const mkdirp = require('mkdirp')
-const { ExtensionInstaller } = require('@ali/ide-extension-installer')
 const Promise = require('bluebird')
+const urllib = require('urllib')
+const fs = require('fs-extra')
+const os = require('os')
+const got = require('got')
+const { v4 } = require('uuid')
+const awaitEvent = require('await-event')
+const yauzl = require('yauzl')
 
-const pkg = require('../package.json')
-const account = require('./account')()
+const api = 'https://open-vsx.org/api/'
 
 // 放置 kaitian extension 的目录
 const targetDir = path.resolve(__dirname, '../extensions')
 
-const extensionInstaller = new ExtensionInstaller({
-  accountId: account.accountId,
-  masterKey: account.masterKey,
-  frameworkVersion: pkg.version,
-  dist: targetDir,
-  ignoreIncreaseCount: true
-})
+const installExtension = async (namespace, name, version) => {
+  const path = version
+    ? `${namespace}/${name}/${version}`
+    : `${namespace}/${name}`
+  const res = await urllib.request(`${api}${path}`, {
+    dataType: 'json',
+    timeout: 100000
+  })
+  if (res.data.files && res.data.files.download) {
+    const { targetDirName, tmpZipFile } = await downloadExtension(
+      res.data.files.download,
+      namespace,
+      name
+    )
+    // 解压插件
+    await unzipFile(targetDir, targetDirName, tmpZipFile)
+    rimraf.sync(tmpZipFile)
+  }
+}
 
 // vscode extension 的 tar 包 oss 地址
 const { extensions } = require('../config/extensions')
@@ -27,16 +44,16 @@ const downloadVscodeExtensions = async () => {
   mkdirp.sync(targetDir)
 
   for (const publisher of Object.keys(extensions)) {
-    Promise.map(
+    await Promise.map(
       extensions[publisher],
-      async (item) => {
+      async item => {
         const { name, version } = item
         console.log('开始安装：%s', name)
-        await extensionInstaller.install({
-          publisher,
-          name,
-          version
-        })
+        try {
+          await installExtension(publisher, name, version)
+        } catch (e) {
+          console.log(`${name} 插件安装失败: ${e.message}`)
+        }
       },
       { concurrency: 3 }
     )
@@ -45,8 +62,133 @@ const downloadVscodeExtensions = async () => {
   console.log('安装完毕')
 }
 
+async function downloadExtension(url, namespace, extensionName) {
+  const tmpPath = path.join(os.tmpdir(), 'extension', v4())
+  const tmpZipFile = path.join(tmpPath, path.basename(url))
+  await fs.mkdirp(tmpPath)
+
+  const tmpStream = fs.createWriteStream(tmpZipFile)
+  const data = await got.default.stream(url, { timeout: 100000 })
+
+  data.pipe(tmpStream)
+  await Promise.race([awaitEvent(data, 'end'), awaitEvent(data, 'error')])
+  tmpStream.close()
+
+  const targetDirName = path.basename(`${namespace}.${extensionName}`)
+
+  return { tmpZipFile, targetDirName }
+}
+
+function openZipStream(zipFile, entry) {
+  return new Promise((resolve, reject) => {
+    zipFile.openReadStream(entry, (error, stream) => {
+      if (error) {
+        reject(error)
+      } else {
+        resolve(stream)
+      }
+    })
+  })
+}
+
+function modeFromEntry(entry) {
+  const attr = entry.externalFileAttributes >> 16 || 33188
+
+  return [448 /* S_IRWXU */, 56 /* S_IRWXG */, 7 /* S_IRWXO */]
+    .map(mask => attr & mask)
+    .reduce((a, b) => a + b, attr & 61440 /* S_IFMT */)
+}
+
+function createZipFile(zipFilePath) {
+  return new Promise((resolve, reject) => {
+    yauzl.open(zipFilePath, { lazyEntries: true }, (err, zipfile) => {
+      if (err) {
+        reject(err)
+      }
+      resolve(zipfile)
+    })
+  })
+}
+
+function unzipFile(dist, targetDirName, tmpZipFile) {
+  const sourcePathRegex = new RegExp('^extension')
+  return new Promise(async (resolve, reject) => {
+    try {
+      const extensionDir = path.join(dist, targetDirName)
+      // 创建插件目录
+      await fs.mkdirp(extensionDir)
+
+      const zipFile = await createZipFile(tmpZipFile)
+      zipFile.readEntry()
+      zipFile.on('error', e => {
+        reject(e)
+      })
+
+      zipFile.on('close', () => {
+        if (!fs.pathExistsSync(path.join(extensionDir, 'package.json'))) {
+          reject(new Error(`Download Error: ${extensionDir}/package.json`))
+          return
+        }
+        fs.remove(tmpZipFile).then(() => resolve(extensionDir))
+      })
+
+      zipFile.on('entry', entry => {
+        if (!sourcePathRegex.test(entry.fileName)) {
+          zipFile.readEntry()
+          return
+        }
+        let fileName = entry.fileName.replace(sourcePathRegex, '')
+
+        if (/\/$/.test(fileName)) {
+          const targetFileName = path.join(extensionDir, fileName)
+          fs.mkdirp(targetFileName).then(() => zipFile.readEntry())
+          return
+        }
+
+        let originalFileName
+        // 在Electron中，如果解包的文件中存在.asar文件，会由于Electron本身的bug导致无法对.asar创建writeStream
+        // 此处先把.asar文件写到另外一个目标文件中，完成后再进行重命名
+        if (fileName.endsWith('.asar') && this.options.isElectronEnv) {
+          originalFileName = fileName
+          fileName += '_prevent_bug'
+        }
+        const readStream = openZipStream(zipFile, entry)
+        const mode = modeFromEntry(entry)
+        readStream.then(stream => {
+          const dirname = path.dirname(fileName)
+          const targetDirName = path.join(extensionDir, dirname)
+          if (targetDirName.indexOf(extensionDir) !== 0) {
+            throw new Error(`invalid file path ${targetDirName}`)
+          }
+          const targetFileName = path.join(extensionDir, fileName)
+
+          fs.mkdirp(targetDirName).then(() => {
+            const writerStream = fs.createWriteStream(targetFileName, { mode })
+            writerStream.on('close', () => {
+              if (originalFileName) {
+                // rename .asar, if filename has been modified
+                fs.renameSync(
+                  targetFileName,
+                  path.join(extensionDir, originalFileName)
+                )
+              }
+              zipFile.readEntry()
+            })
+            stream.on('error', err => {
+              throw err
+            })
+            stream.pipe(writerStream)
+          })
+        })
+      })
+    } catch (err) {
+      reject(err)
+    }
+  })
+}
+
 // 执行并捕捉异常
-downloadVscodeExtensions().catch((e) => {
+downloadVscodeExtensions().catch(e => {
   console.trace(e)
   process.exit(128)
 })
